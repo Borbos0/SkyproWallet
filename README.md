0db5a5ab3f014b1f6fda9eda4478b892bd28e09f
Daniil Ermolin <daniil.ermolin@itlab-pro.ru>
2025-05-23 21:03:18 +0300

    init project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Skypro Wallet
+
+Учебный проект для командной работы
+
+## Available Scripts
+
+Установка зависимостей
+
+### `npm install`
+
+Запуск проекта
+
+### `npm start`
+
+eslint
+
+### `npm lint`
+
+prettier
+
+### `npm format`
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex w-full h-60 bg-black items-center justify-center text-white text-24">
+      <div>
+        Начальная страница
+      </div>
+    </div>
+  );
+}
+
+export default App;
