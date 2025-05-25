f5710f027068b0ad351083dc9c8b4c9df4f8a564
Daniil Ermolin <daniil.ermolin@itlab-pro.ru>
2025-05-25 16:29:21 +0300

    добавление компонентов и страниц

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ExpensesPage from './pages/ExpensesPage';
+import AnalyticsPage from './pages/AnalyticsPage';
 
 function App() {
+  const isAuth = false;
+
   return (
-    <div className="flex w-full h-60 bg-black items-center justify-center text-white text-24">
-      <div>
-        Начальная страница
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Публичные роуты */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Приватные роуты */}
+        <Route
+          path="/expenses"
+          element={isAuth ? <ExpensesPage /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/analytics"
+          element={isAuth ? <AnalyticsPage /> : <Navigate to="/login" replace />}
+        />
+
+        {/* Редирект по умолчанию */}
+        <Route
+          path="/"
+          element={
+            isAuth ? <Navigate to="/expenses" replace /> : <Navigate to="/login" replace />
+          }
+        />
+
+        {/* Страница 404 */}
+        <Route path="*" element={<Navigate to="/error" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Register Page</h1>
+      <p>Тут будет форма регистрации</p>
+    </div>
+  );
+  
+  export default RegisterPage;
+  
