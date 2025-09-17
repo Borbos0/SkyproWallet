56c0bda34660dc7b54ec0dcb4ee9bab518e8879a
Daniil Ermolin <daniil.ermolin@itlab-pro.ru>
2025-09-17 17:20:54 +0300

    Добавил апи запросы на период транзакции. замокал период и переделал таблицу

---
+++ src/api/transactions.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const BASE_URL = "https://wedev-api.sky.pro/api/transactions";
+
+// Хелпер для токена
+const getAuthHeader = () => {
+  const token = localStorage.getItem("token");
+  return token ? { Authorization: `Bearer ${token}` } : {};
+};
+
+// Получить список транзакций (сортировка/фильтрация опционально)
+export const getTransactions = async (options = {}) => {
+  const { sortBy, filterBy } = options;
+
+  const params = {};
+  if (sortBy) params.sortBy = sortBy;
+  if (filterBy) {
+    params.filterBy = Array.isArray(filterBy) ? filterBy.join(",") : filterBy;
+  }
+
+  const res = await axios.get(BASE_URL, {
+    params,
+    headers: {
+      ...getAuthHeader(),
+      'content-type': '',
+    },
+  });
+
+  return res.data;
+};
+
+// Получить транзакции за период
+export const getTransactionsByPeriod = async ({ start, end }) => {
+  const res = await axios.post(
+    `${BASE_URL}/period`,
+    { start, end },
+    {
+      headers: {
+        ...getAuthHeader(),
+        'content-type': '',
+      },
+    }
+  );
+
+  return res.data;
+};
