6b209a3a04f0a8a07f7960054de69734ff194c0c
AlexxGutt <alex.gutt89@gmail.com>
2025-09-19 23:25:15 +0300

    Вертка модуля

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,29 +3,50 @@ const Expenses = () => (
   <div className="expenses-table w-[789px] h-[618px] flex-col rounded-[30px] shadow-[0px_20px_67px_-12px_rgba(0,0,0,0.13)] bg-white mt-[116px]">
   <div className="expenses-table__header">
     <div className="expenses-table__line expenses-table__line--first flex ml-8">
-      <h2 className="expenses-table__title">Таблица расходов</h2>
+      <h2 className="expenses-table__title text-black font-montserrat text-2xl font-bold leading-[29px] tracking-normal text-center">Таблица расходов</h2>
       <div className="expenses-table__filters flex">
-        <div className="expenses-table__filter">Фильтровать по категории</div>
-        <div className="expenses-table__filter">Сортировать по дате</div>
+        <div className="expenses-table__filter text-[12px]">Фильтровать по категории</div>
+        <div className="expenses-table__filter text-[12px]">Сортировать по дате</div>
       </div>
     </div>
-    <div className="expenses-table__line expenses-table__line--second font-montserrat font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center gap-32 text-[#999] ml-[32px] pr-[69px]">
-      <div className="expenses-table__column-title text-[12px] w-[141px] h-[15px]">Описание</div>
-      <div className="expenses-table__column-title text-[12px] w-[141px] h-[15px]">Категория</div>
-      <div className="expenses-table__column-title text-[12px] w-[141px] h-[15px]">Дата</div>
-      <div className="expenses-table__column-title text-[12px] w-[141px] h-[15px]">Сумма</div>
+    <div className="font-montserrat font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center text-[#999] ml-[32px] pr-[69px]">
+      <div className="text-[12px] w-[141px] h-[15px]">Описание</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Категория</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Дата</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Сумма</div>
     </div>
     <div className="line w-[789px] h-0 border-t border-t-gray-400 border-t-[0.5px] mt-[5.75px] "></div>
   </div>
-  
-  <div className="expenses-table__row font-montserrat font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center gap-32 ml-[32px]">
-    <div className="expenses-table__cell expenses-table__cell--description text-[12px] w-[141px] h-[15px]">Пятерочка</div>
-    <div className="expenses-table__cell expenses-table__cell--category text-[12px] w-[141px] h-[15px]">Еда</div>
-    <div className="expenses-table__cell expenses-table__cell--date text-[12px] w-[141px] h-[15px]">03.07.2024</div>
-    <div className="expenses-table__cell expenses-table__cell--money text-[12px] w-[141px] h-[15px]">3500</div>
-    <div className="expenses-table__actions flex w-[36px] h-[12px]">
-      <img className="expenses-table__icon" src="/img_edit.svg" alt="Редактировать" />
-      <img className="expenses-table__icon" src="/img_del.svg" alt="Удалить" />
+  <div className="w-[723px] h-[479px] flex flex-col justify-start items-start gap-[14px] pt-[18.25px]">
+    <div className="font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center ml-[32px]">
+      <div className="text-[12px] w-[141px] h-[15px]">Пятерочка</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Еда</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">03.07.2024</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">3 500р</div>
+      <div className="flex w-[36px] h-[12px] ml-[32px]">
+        <img className="w-[12px] h-[12px] mr-[12px]" src="/img_edit.svg" alt="Редактировать" />
+        <img className="w-[12px] h-[12px] mr-[1px]" src="/img_del.svg" alt="Удалить" />
+      </div>
+    </div>
+    <div className="font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center ml-[32px]">
+      <div className="text-[12px] w-[141px] h-[15px]">Пятерочка</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Еда</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">03.07.2024</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">3 500р</div>
+      <div className="flex w-[36px] h-[12px] ml-[32px]">
+        <img className="w-[12px] h-[12px] mr-[12px]" src="/img_edit.svg" alt="Редактировать" />
+        <img className="w-[12px] h-[12px] mr-[1px]" src="/img_del.svg" alt="Удалить" />
+      </div>
+    </div>
+    <div className="font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center ml-[32px]">
+      <div className="text-[12px] w-[141px] h-[15px]">Пятерочка</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Еда</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">03.07.2024</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">3 500р</div>
+      <div className="flex w-[36px] h-[12px] ml-[32px]">
+        <img className="w-[12px] h-[12px] mr-[12px]" src="/img_edit.svg" alt="Редактировать" />
+        <img className="w-[12px] h-[12px] mr-[1px]" src="/img_del.svg" alt="Удалить" />
+      </div>
     </div>
   </div>
 </div>
