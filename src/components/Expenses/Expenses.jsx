d19812fae0256c9be325a582ddd4a061bc9412a1
AlexxGutt <alex.gutt89@gmail.com>
2025-09-19 20:43:04 +0300

    Стили Tilewind

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,34 +1,35 @@
 const Expenses = () => (
   <div>
-  <div class="expenses-table">
-  <div class="expenses-table__header">
-    <div class="expenses-table__line expenses-table__line--first">
-      <h1 class="expenses-table__title">Таблица расходов</h1>
-      <div class="expenses-table__filters">
-        <div class="expenses-table__filter">Фильтровать по категории</div>
-        <div class="expenses-table__filter">Сортировать по дате</div>
+  <div className="expenses-table w-[789px] h-[618px] flex-col rounded-[30px] shadow-[0px_20px_67px_-12px_rgba(0,0,0,0.13)] bg-white">
+  <div className="expenses-table__header">
+    <div className="expenses-table__line expenses-table__line--first flex ml-8">
+      <h1 className="expenses-table__title">Таблица расходов</h1>
+      <div className="expenses-table__filters flex">
+        <div className="expenses-table__filter">Фильтровать по категории</div>
+        <div className="expenses-table__filter">Сортировать по дате</div>
       </div>
     </div>
-    <div class="expenses-table__line expenses-table__line--second">
-      <div class="expenses-table__column-title">Описание</div>
-      <div class="expenses-table__column-title">Категория</div>
-      <div class="expenses-table__column-title">Дата</div>
-      <div class="expenses-table__column-title">Сумма</div>
+    <div className="expenses-table__line expenses-table__line--second w-[723px] h-[15px] flex justify-start items-center gap-32 text-[#999] ml-8">
+      <div className="expenses-table__column-title w-[141px] h-[15px]">Описание</div>
+      <div className="expenses-table__column-title w-[141px] h-[15px]">Категория</div>
+      <div className="expenses-table__column-title w-[141px] h-[15px]">Дата</div>
+      <div className="expenses-table__column-title w-[141px] h-[15px]">Сумма</div>
     </div>
+    <div className="line w-[789px] h-0 border-t border-t-gray-400 border-t-[0.5px] mt-[7.5px]"></div>
   </div>
   
-  <div class="expenses-table__row">
-    <div class="expenses-table__cell expenses-table__cell--description">Пятерочка</div>
-    <div class="expenses-table__cell expenses-table__cell--category">Еда</div>
-    <div class="expenses-table__cell expenses-table__cell--date">03.07.2024</div>
-    <div class="expenses-table__cell expenses-table__cell--money">3500</div>
-    <div class="expenses-table__actions">
-      <img class="expenses-table__icon" src="/img_edit.svg" alt="Редактировать" />
-      <img class="expenses-table__icon" src="/img_del.svg" alt="Удалить" />
+  <div className="expenses-table__row flex ml-8">
+    <div className="expenses-table__cell expenses-table__cell--description">Пятерочка</div>
+    <div className="expenses-table__cell expenses-table__cell--category">Еда</div>
+    <div className="expenses-table__cell expenses-table__cell--date">03.07.2024</div>
+    <div className="expenses-table__cell expenses-table__cell--money">3500</div>
+    <div className="expenses-table__actions flex">
+      <img className="expenses-table__icon" src="/img_edit.svg" alt="Редактировать" />
+      <img className="expenses-table__icon" src="/img_del.svg" alt="Удалить" />
     </div>
   </div>
 </div>
-    {/* <h2 className="text-lg font-semibold mb-2">Список расходов</h2>
+    {/* <h2 classNameNameName="text-lg font-semibold mb-2">Список расходов</h2>
     <p>Тут будет список расходов пользователя</p> */}
   </div>
 );
