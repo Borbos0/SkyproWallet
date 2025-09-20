ee906f6ef19f967fa64568afb1e7fd035f98f6ce
AlexxGutt <alex.gutt89@gmail.com>
2025-09-20 18:33:27 +0300

    1-я модалка

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,9 +4,41 @@ const Expenses = () => (
   <div className="expenses-table__header">
     <div className="flex items-baseline justify-between ml-[32px]">
       <h2 className="font-montserrat text-2xl font-bold leading-[29px] tracking-normal text-center mt-[32px] mb-[32px]">Таблица расходов</h2>
-      <div className="flex items-center">
-        <div className="text-[12px] font-normal leading-[150%] tracking-normal ml-[130px]">Фильтровать по категории</div>
-        <img className="h-[7px] w-[7px] ml-[8px]" src="/polygon 3.svg" alt="" />
+      <div className="items flex items-center">
+      <div className="flex items-baseline relative">
+          <div className="text-[12px] font-normal leading-[150%] tracking-normal ml-[130px]">Фильтровать по категории</div>
+            <div className="text-[#1fa46c] font-montserrat text-[12px] font-semibold leading-[150%] text-center border-b-[0.5px] border-b-[#1fa46c] ml-[6.5px]">еда</div>
+            <img className="h-[7px] w-[7px] ml-[8px]" src="/polygon 3.svg" alt="" />
+            {/* модальное окно "фильтр по категориям" */}
+              <div className="absolute right-0 top-[24px] w-[176px] h-[240px] p-[12px] box-border border-[0.5px] border-[#999999] rounded-[6px] shadow-[0px_20px_67px_-12px_rgba(0,0,0,0.13)] bg-white">
+                <div className="w-[152px] h-[216px] flex flex-col items-start gap-1.5">
+                  <div className="flex rounded-[30px] bg-[#dbffe9] px-[20px] py-[8px]">
+                    <img className="w-[14px] h-[14px]" src="/bag.svg" alt="" />
+                    <p className="text-[#1fa46c] font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Еда</p>
+                  </div>
+                  <div className="flex rounded-[30px] bg-gray-100 px-[20px] py-[8px]">
+                    <img className="w-[14px] h-[14px]" src="/car.svg" alt="" />
+                    <p className="text-black font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Транспорт</p>
+                  </div>
+                  <div className="flex rounded-[30px] bg-gray-100 px-[20px] py-[8px]">
+                    <img className="w-[14px] h-[14px]" src="/house.svg" alt="" />
+                    <p className="text-black font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Жилье</p>
+                  </div>
+                  <div className="flex rounded-[30px] bg-gray-100 px-[20px] py-[8px]">
+                    <img className="w-[14px] h-[14px]" src="/gameboy.svg" alt="" />
+                    <p className="text-black font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Развлечения</p>
+                  </div>
+                  <div className="flex rounded-[30px] bg-gray-100 px-[20px] py-[8px]">
+                    <img className="w-[14px] h-[14px]" src="/vuesax_bold_teacher.svg" alt="" />
+                    <p className="text-black font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Образование</p>
+                  </div>
+                  <div className="flex rounded-[30px] bg-gray-100 px-[20px] py-[8px]">
+                   <img className="w-[14px] h-[14px]" src="/message-text.svg" alt="" />
+                   <p className="text-black font-montserrat text-xs font-normal leading-[15px] text-center ml-[12px]">Другое</p>
+                 </div>
+                </div>
+              </div>
+      </div>
         <div className="text-[12px] font-normal leading-[150%] tracking-normal ml-[24px]">Сортировать по</div>
         <div className="text-[12px] ml-[4px]">дате</div>
         <img className="h-[7px] w-[7px] ml-[8px] mr-[34px]" src="/polygon 3.svg" alt="" />
