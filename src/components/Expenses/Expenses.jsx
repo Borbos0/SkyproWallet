21e3f7dda9232077415f11d67a7dcbf0907666d3
AlexxGutt <alex.gutt89@gmail.com>
2025-09-20 16:17:17 +0300

    Подогнал модуль по макету (далее модальные окна)

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,12 +1,15 @@
 const Expenses = () => (
   <div>
-  <div className="expenses-table w-[789px] h-[618px] flex-col rounded-[30px] shadow-[0px_20px_67px_-12px_rgba(0,0,0,0.13)] bg-white mt-[116px]">
+  <div className="w-[789px] h-[618px] flex-col rounded-[30px] shadow-[0px_20px_67px_-12px_rgba(0,0,0,0.13)] bg-white mt-[116px]">
   <div className="expenses-table__header">
-    <div className="expenses-table__line expenses-table__line--first flex ml-8">
-      <h2 className="expenses-table__title text-black font-montserrat text-2xl font-bold leading-[29px] tracking-normal text-center">Таблица расходов</h2>
-      <div className="expenses-table__filters flex">
-        <div className="expenses-table__filter text-[12px]">Фильтровать по категории</div>
-        <div className="expenses-table__filter text-[12px]">Сортировать по дате</div>
+    <div className="flex items-baseline justify-between ml-[32px]">
+      <h2 className="font-montserrat text-2xl font-bold leading-[29px] tracking-normal text-center mt-[32px] mb-[32px]">Таблица расходов</h2>
+      <div className="flex items-center">
+        <div className="text-[12px] font-normal leading-[150%] tracking-normal ml-[130px]">Фильтровать по категории</div>
+        <img className="h-[7px] w-[7px] ml-[8px]" src="/polygon 3.svg" alt="" />
+        <div className="text-[12px] font-normal leading-[150%] tracking-normal ml-[24px]">Сортировать по</div>
+        <div className="text-[12px] ml-[4px]">дате</div>
+        <img className="h-[7px] w-[7px] ml-[8px] mr-[34px]" src="/polygon 3.svg" alt="" />
       </div>
     </div>
     <div className="font-montserrat font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center text-[#999] ml-[32px] pr-[69px]">
@@ -29,10 +32,10 @@ const Expenses = () => (
       </div>
     </div>
     <div className="font-normal leading-[150%] tracking-[0px] w-[723px] h-[15px] flex justify-start items-center ml-[32px]">
-      <div className="text-[12px] w-[141px] h-[15px]">Пятерочка</div>
-      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Еда</div>
+      <div className="text-[12px] w-[141px] h-[15px]">Яндекс такси</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">Транспорт</div>
       <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">03.07.2024</div>
-      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">3 500р</div>
+      <div className="text-[12px] w-[141px] h-[15px] ml-[32px]">750р</div>
       <div className="flex w-[36px] h-[12px] ml-[32px]">
         <img className="w-[12px] h-[12px] mr-[12px]" src="/img_edit.svg" alt="Редактировать" />
         <img className="w-[12px] h-[12px] mr-[1px]" src="/img_del.svg" alt="Удалить" />
